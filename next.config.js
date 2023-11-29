1ddc2807b1723a36ee45167c0907310017e3e111
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-11-28 22:12:26 -0300

    chore(config): Set pageExtensions for app routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["api.ts", "page.tsx"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
